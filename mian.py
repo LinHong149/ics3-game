6b3306e0de62baa0570094f4e07538d7512d932d
LinHong <linlin.hong1@gmail.com>
2024-06-05 13:50:04 -0400

    Player movement and currency counter

--- mian.py
+++ mian.py
@@ -1,27 +1,72 @@
-# Example file showing a basic pygame "game loop"
+# Import libraries
 import pygame
+from plant import plant
 
-# pygame setup
+
+# Declare variables
+screenWidth = 1280
+screenHeight = 800
+day = 1
+currency = 1000
+
+
+# Pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption('Title')
+screen = pygame.display.set_mode((screenWidth, screenHeight))
 clock = pygame.time.Clock()
 running = True
 
+
+# Setup text
+def updateText():
+    global currencyText
+    global currencyRect
+    font = pygame.font.SysFont('Arial', 32)
+    currencyText = font.render(str(currency), True, (0,255,0))
+    currencyRect = currencyText.get_rect()
+    currencyRect.bottomright = (screenWidth, 32)
+
+
+# Player movement
+def movement(player):
+    pygame.draw.rect(screen, (255, 0 ,0), player)
+    keyPressed = pygame.key.get_pressed()
+    if keyPressed[pygame.K_UP]:
+        player.move_ip(0, -1)
+    elif keyPressed[pygame.K_DOWN]:
+        player.move_ip(0, 1)
+    elif keyPressed[pygame.K_LEFT]:
+        player.move_ip(-1, 0)
+    elif keyPressed[pygame.K_RIGHT]:
+        player.move_ip(1, 0)
+
+
+# Declare game elements
+player = pygame.Rect((300,250, 30, 30))
+# plant1 = plant(stage=1, age=3)
+
+
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+    # Background
+    screen.fill((0,0,0))
+
+    # Player movement
+    movement(player)
+
+    # Update and print text
+    updateText()
+    screen.blit(currencyText, currencyRect)
+
+
+    clock.tick(60)  # limits FPS to 60
+
+    # Allow game to be exited
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-
-    # RENDER YOUR GAME HERE
-
-    # flip() the display to put your work on screen
-    pygame.display.flip()
-
-    clock.tick(60)  # limits FPS to 60
+    
+    pygame.display.update()
+            
 
 pygame.quit()
