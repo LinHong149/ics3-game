03c2932f42b7331acce87c77d5240fcf3e6d344c
LinHong <linlin.hong1@gmail.com>
2024-06-18 22:17:40 -0400

    Updated shop UI

--- src/shop.py
+++ src/shop.py
@@ -8,7 +8,7 @@ class Shop:
         self.currency = currency
         self.items_prices = items_prices
         self.font = font
-        self.sell_button_rect = pygame.Rect(100, 100, 200, 50)
+        self.sell_button_rect = pygame.Rect(SCREEN_WIDTH-300-300, 400, 300, 60)
 
     def draw(self, inventory):
         font = pygame.font.SysFont('arial', 40)
@@ -20,17 +20,23 @@ class Shop:
         self.screen.blit(background, (0, 0))
 
         # Draws shop title
-        title = font.render('Shop', True, (255, 255, 255))
+        title = font.render('Shop', True, WHITE)
         self.screen.blit(title, (SCREEN_WIDTH/2 - title.get_width()/2, SCREEN_HEIGHT/2 - title.get_height()/2 - 200))
 
+        buy_header = font.render('Buy', True, WHITE)
+        self.screen.blit(buy_header, (SCREEN_WIDTH/2 - buy_header.get_width()/2 - 200, SCREEN_HEIGHT/2 - title.get_height()/2 - 50))
+
+        sell_header = font.render('Sell', True, WHITE)
+        self.screen.blit(sell_header, (SCREEN_WIDTH/2 - sell_header.get_width()/2 + 190, SCREEN_HEIGHT/2 - title.get_height()/2 - 50))
+
         # Draw shop buttons
-        buy_dirt_button = pygame.Rect(500,500, 100, 60)
-        buy_carrot_seed_button = pygame.Rect(700,500, 100, 60)
+        buy_dirt_button = pygame.Rect(300,400, 300, 60)
+        buy_carrot_seed_button = pygame.Rect(300,500, 300, 60)
         pygame.draw.rect(self.screen, WHITE, buy_dirt_button, 2)
         pygame.draw.rect(self.screen, WHITE, buy_carrot_seed_button, 2)
 
-        buy_dirt_text = self.font.render("Buy Dirt", True, WHITE)
-        buy_seeds_text = self.font.render("Buy Seeds", True, WHITE)
+        buy_dirt_text = self.font.render("Buy Dirt $20", True, WHITE)
+        buy_seeds_text = self.font.render("Buy Seeds $8", True, WHITE)
         self.screen.blit(buy_dirt_text, (buy_dirt_button.x + 10, buy_dirt_button.y + 10))
         self.screen.blit(buy_seeds_text, (buy_carrot_seed_button.x + 10, buy_carrot_seed_button.y + 10))
         
@@ -45,12 +51,12 @@ class Shop:
                     self.currency.buy(self.items_prices["carrot_seeds"], "carrot_seeds")
 
         # Button for selling crops
-        pygame.draw.rect(self.screen, WHITE, self.sell_button_rect)
+        pygame.draw.rect(self.screen, WHITE, self.sell_button_rect, 2)
         if inventory.get_items().get("carrot", 0) > 0:
-            sell_text = self.font.render("Sell Carrot", True, BLACK)
+            sell_text = self.font.render("Sell Carrot $12", True, WHITE)
             self.screen.blit(sell_text, (self.sell_button_rect.x + 10, self.sell_button_rect.y + 10))
         else:
-            sell_text = self.font.render("No Carrots to Sell", True, BLACK)
+            sell_text = self.font.render("No Carrots", True, WHITE)
             self.screen.blit(sell_text, (self.sell_button_rect.x + 10, self.sell_button_rect.y + 10))
 
 
